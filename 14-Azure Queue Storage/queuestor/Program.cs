f87ec10bf917386b8435c55b6e1cdc6ef672105a
wjacb <wjacb@hotmail.com>
2026-02-27 08:49:08 +0000

    modified:   14-Azure Queue Storage/queuestor/Program.cs

--- 14-Azure Queue Storage/queuestor/Program.cs
+++ 14-Azure Queue Storage/queuestor/Program.cs
@@ -5,96 +5,75 @@ using Azure.Storage.Queues.Models;
 using System;
 using System.Threading.Tasks;
 
-// Create a unique name for the queue
-// TODO: Replace the <YOUR-STORAGE-ACCT-NAME> placeholder 
+
 string queueName = "myqueue-" + Guid.NewGuid().ToString();
 string storageAccountName = "storactname59533458";
 
-// ADD CODE TO CREATE A QUEUE CLIENT AND CREATE A QUEUE
+await CreateQueueAsync(storageAccountName, queueName);
+await SendAndListMessagesAsync(storageAccountName, queueName);
+await UpdateAndListMessagesAsync(storageAccountName, queueName);
+await DeleteMessagesAndQueueAsync(storageAccountName, queueName);
 
-// Create a DefaultAzureCredentialOptions object to exclude certain credentials
-DefaultAzureCredentialOptions options = new()
+static async Task CreateQueueAsync(string storageAccountName, string queueName)
 {
-    ExcludeEnvironmentCredential = true,
-    ExcludeManagedIdentityCredential = true
-};
+    var options = new DefaultAzureCredentialOptions
+    {
+        ExcludeEnvironmentCredential = true,
+        ExcludeManagedIdentityCredential = true
+    };
+    var queueClient = new QueueClient(
+        new Uri($"https://{storageAccountName}.queue.core.windows.net/{queueName}"),
+        new DefaultAzureCredential(options));
+    Console.WriteLine($"Creating queue: {queueName}");
+    await queueClient.CreateAsync();
+    Console.WriteLine("Queue created, press Enter to continue...");
+    Console.ReadLine();
+}
 
-// Instantiate a QueueClient to create and interact with the queue
-QueueClient queueClient = new QueueClient(
-    new Uri($"https://{storageAccountName}.queue.core.windows.net/{queueName}"),
-    new DefaultAzureCredential(options));
-
-Console.WriteLine($"Creating queue: {queueName}");
-
-// Create the queue
-await queueClient.CreateAsync();
-
-Console.WriteLine("Queue created, press Enter to add messages to the queue...");
-Console.ReadLine();
-
-
-// ADD CODE TO SEND AND LIST MESSAGES
-
-// Create a DefaultAzureCredentialOptions object to exclude certain credentials
-DefaultAzureCredentialOptions options = new()
+static async Task SendAndListMessagesAsync(string storageAccountName, string queueName)
 {
-    ExcludeEnvironmentCredential = true,
-    ExcludeManagedIdentityCredential = true
-};
+    var options = new DefaultAzureCredentialOptions
+    {
+        ExcludeEnvironmentCredential = true,
+        ExcludeManagedIdentityCredential = true
+    };
+    var queueClient = new QueueClient(
+        new Uri($"https://{storageAccountName}.queue.core.windows.net/{queueName}"),
+        new DefaultAzureCredential(options));
+    Console.WriteLine($"Sending message to queue: {queueName}");
+    // Example: await queueClient.SendMessageAsync("Hello, Azure Queue!");
+    Console.WriteLine("Message sent, press Enter to continue...");
+    Console.ReadLine();
+}
 
-// Instantiate a QueueClient to create and interact with the queue
-QueueClient queueClient = new QueueClient(
-    new Uri($"https://{storageAccountName}.queue.core.windows.net/{queueName}"),
-    new DefaultAzureCredential(options));
-
-Console.WriteLine($"Creating queue: {queueName}");
-
-// Create the queue
-await queueClient.CreateAsync();
-
-Console.WriteLine("Queue created, press Enter to add messages to the queue...");
-Console.ReadLine();
-
-// ADD CODE TO UPDATE A MESSAGE AND LIST MESSAGES
-
-// Create a DefaultAzureCredentialOptions object to exclude certain credentials
-DefaultAzureCredentialOptions options = new()
+static async Task UpdateAndListMessagesAsync(string storageAccountName, string queueName)
 {
-    ExcludeEnvironmentCredential = true,
-    ExcludeManagedIdentityCredential = true
-};
+    var options = new DefaultAzureCredentialOptions
+    {
+        ExcludeEnvironmentCredential = true,
+        ExcludeManagedIdentityCredential = true
+    };
+    var queueClient = new QueueClient(
+        new Uri($"https://{storageAccountName}.queue.core.windows.net/{queueName}"),
+        new DefaultAzureCredential(options));
+    Console.WriteLine($"Updating and listing messages in queue: {queueName}");
+    // Example: update or list messages here
+    Console.WriteLine("Messages updated/listed, press Enter to continue...");
+    Console.ReadLine();
+}
 
-// Instantiate a QueueClient to create and interact with the queue
-QueueClient queueClient = new QueueClient(
-    new Uri($"https://{storageAccountName}.queue.core.windows.net/{queueName}"),
-    new DefaultAzureCredential(options));
-
-Console.WriteLine($"Creating queue: {queueName}");
-
-// Create the queue
-await queueClient.CreateAsync();
-
-Console.WriteLine("Queue created, press Enter to add messages to the queue...");
-Console.ReadLine();
-
-// ADD CODE TO DELETE MESSAGES AND THE QUEUE
-
-// Create a DefaultAzureCredentialOptions object to exclude certain credentials
-DefaultAzureCredentialOptions options = new()
+static async Task DeleteMessagesAndQueueAsync(string storageAccountName, string queueName)
 {
-    ExcludeEnvironmentCredential = true,
-    ExcludeManagedIdentityCredential = true
-};
-
-// Instantiate a QueueClient to create and interact with the queue
-QueueClient queueClient = new QueueClient(
-    new Uri($"https://{storageAccountName}.queue.core.windows.net/{queueName}"),
-    new DefaultAzureCredential(options));
-
-Console.WriteLine($"Creating queue: {queueName}");
-
-// Create the queue
-await queueClient.CreateAsync();
-
-Console.WriteLine("Queue created, press Enter to add messages to the queue...");
-Console.ReadLine();
+    var options = new DefaultAzureCredentialOptions
+    {
+        ExcludeEnvironmentCredential = true,
+        ExcludeManagedIdentityCredential = true
+    };
+    var queueClient = new QueueClient(
+        new Uri($"https://{storageAccountName}.queue.core.windows.net/{queueName}"),
+        new DefaultAzureCredential(options));
+    Console.WriteLine($"Deleting messages and queue: {queueName}");
+    // Example: delete messages and then delete the queue
+    Console.WriteLine("Messages and queue deleted, press Enter to finish...");
+    Console.ReadLine();
+}
